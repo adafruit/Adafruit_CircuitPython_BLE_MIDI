67e1313c3f480529e92698622b767f97967476d5
Scott Shawcroft <scott@tannewt.org>
2020-03-27 13:34:46 -0700

    Works before black and lint

--- adafruit_ble_midi.py
+++ adafruit_ble_midi.py
@@ -47,7 +47,7 @@ class _MidiCharacteristic(ComplexCharacteristic):
 
     def __init__(self):
         super().__init__(properties=Characteristic.WRITE_NO_RESPONSE | Characteristic.READ | Characteristic.NOTIFY,
-                         read_perm=Attribute.OPEN, write_perm=Attribute.OPEN,
+                         read_perm=Attribute.ENCRYPT_NO_MITM, write_perm=Attribute.ENCRYPT_NO_MITM,
                          max_length=512,
                          fixed_length=False)
 
@@ -63,7 +63,7 @@ class MIDIService(Service):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._in_buffer = bytearray(self._raw.packet_length)
+        self._in_buffer = bytearray(self._raw.packet_size)
         self._out_buffer = None
         shared_buffer = memoryview(bytearray(4))
         self._buffers = [None, shared_buffer[:1], shared_buffer[:2], shared_buffer[:3], shared_buffer[:4]]
@@ -72,10 +72,46 @@ class MIDIService(Service):
         self._message_target_length = None
         self._message_length = 0
         self._pending_realtime = None
+        self._in_length = 0
+        self._in_index = 1
+        self._last_read = time.monotonic_ns() // 1000000
+        self._last_low_ms = None
+        self._last_data = True
+
+    def readinto(self, buf, length):
+        i = 0
+        while i < length:
+            if self._in_index < self._in_length:
+                byte = self._in_buffer[self._in_index]
+                if self._last_data and byte & 0x80 != 0:
+                    # Maybe manage timing here. Not done now because we're likely slower than we
+                    # need to be already.
+                    # low_ms = byte & 0x7f
+                    # print("low", low_ms)
+                    self._in_index += 1
+                    self._last_data = False
+                    continue
+                self._in_index += 1
+                self._last_data = True
+                buf[i] = byte
+                i += 1
+            else:
+                if len(self._in_buffer) < self._raw.packet_size:
+                    self._in_buffer = bytearray(self._raw.packet_size)
+                self._in_length = self._raw.readinto(self._in_buffer)
+                if self._in_length == 0:
+                    break
+                # high_ms = self._in_buffer[0] & 0x3f
+                # print("high", high_ms)
+                self._in_index = 1
+                self._last_data = True
+
+        return i
 
     def read(self, length):
-        self._raw.read(self._in_buffer)
-        return None
+        result = bytearray(length)
+        i = self.readinto(result, length)
+        return result[:i]
 
     def write(self, buf, length):
         timestamp_ms = time.monotonic_ns() // 1000000
@@ -89,12 +125,12 @@ class MIDIService(Service):
                     b = self._buffers[2]
                     b[0] = 0x80 | (timestamp_ms & 0x7f)
                     b[1] = 0xf7
-                    self._raw.write(b, self._header)
+                    self._raw.write(b, header=self._header)
                     self._in_sysex = data == 0xf7
                 else:
                     b = self._buffers[1]
                     b[0] = data
-                    self._raw.write(b, self._header)
+                    self._raw.write(b, header=self._header)
             elif command:
                 self._in_sysex = data == 0xf0
                 b = self._buffers[2]
@@ -118,8 +154,9 @@ class MIDIService(Service):
                 self._out_buffer[self._message_length] = data
                 self._message_length += 1
                 if self._message_target_length == self._message_length:
-                    self._raw.write(self._out_buffer, self._header)
-                    if _pending_realtime:
-                        self._raw.write(self._pending_realtime, self._header)
+                    self._raw.write(self._out_buffer, header=self._header)
+                    if self._pending_realtime:
+                        self._raw.write(self._pending_realtime, header=self._header)
                         self._pending_realtime = None
                     self._message_target_length = None
+            i += 1
